e29258b85d566d300ac41d11fa58fa455c4e9b82
Son Tung <binlabum123@gmail.com>
2025-11-19 00:50:18 +1030

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
